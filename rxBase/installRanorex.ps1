8abd4e027a616ac670b871c44c85c5b5a98d69b5
Christian Breitwieser <cbreit@ranorex.com>
2017-10-06 15:00:37 +0200

    Automatic vcredist dependency installation Pushed default ranorex version to 7.2

--- rxBase/installRanorex.ps1
+++ rxBase/installRanorex.ps1
@@ -2,13 +2,33 @@ param(
   [Parameter(Mandatory=$true)][String]$zipPackage
 )
 
+
+function installVCRedist()
+{
+  param ( [string]$exe = $(throw "Specify the vcredist exe" ))
+  Write-Host "Installing: " $exe
+  Start-Process -wait ($exe) -ArgumentList '/quiet', '/install'
+}
+
+
 $extractFolder = ".\rxTmp\"
 
 Expand-Archive -LiteralPath $zipPackage -DestinationPath $extractFolder -Force
 
-Start-Process -wait ($extractFolder + "vcredist_x64_14_Ranorex\vcredist_x64.exe") -ArgumentList '/quiet', '/install'
-Start-Process -wait ($extractFolder + "vcredist_x86_14_Ranorex\vcredist_x86.exe") -ArgumentList '/quiet', '/install'
 
+# Install dependencies
+#######################
+
+$vcredistPattern = $extractFolder + "vcredist*\*.exe"
+$vcredistInstallers = Get-ChildItem $vcredistPattern -Recurse | Select-Object FullName
+
+$vcredistInstallers | ForEach-Object {installVCRedist($($_.FullName))}
+
+
+# Install Ranorex
+#######################
+
+Write-Host "Installing Ranorex..."
 $msi = Get-ChildItem -Path ($extractFolder + 'Ranorex-*msi')
 Start-Process -wait msiexec -ArgumentList '/i', $msi, '/quiet', 'ADDLOCAL="MainFeature"'
 
