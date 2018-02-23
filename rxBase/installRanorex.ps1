ff63102ecf769abe9580b58229d1530ef0373d24
Christian Breitwieser <cbreitwieser@ranorex.com>
2018-02-23 11:54:53 +0100

    Cleanup

--- rxBase/installRanorex.ps1
+++ rxBase/installRanorex.ps1
@@ -2,29 +2,11 @@ param(
   [Parameter(Mandatory=$true)][String]$zipPackage
 )
 
-
-function installVCRedist()
-{
-  param ( [string]$exe = $(throw "Specify the vcredist exe" ))
-  Write-Host "Installing: " $exe
-  Start-Process -Wait ($exe) -ArgumentList '/quiet', '/install'
-}
-
 $extractFolder = ".\rxTmp\"
-
 Expand-Archive -LiteralPath $zipPackage -DestinationPath $extractFolder -Force
-
 
 # Install dependencies
 #######################
-
-#$vcredistPattern = $extractFolder + "vcredist*\*.exe"
-#$vcredistInstallers = Get-ChildItem $vcredistPattern -Recurse | Select-Object FullName
-
-#$vcredistInstallers | ForEach-Object {installVCRedist($($_.FullName))}
-
-
-#.\installChoco.ps1
 
 $Packages = 'vcredist2008','vcredist2010','vcredist2013'
 
